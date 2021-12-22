b9f9b2666c7d14f1e9f793121d082ce6558149d1
hanhyunsoo <gustn4563@gmail.com>
2021-12-22 14:47:14 +0900

    Start Project, setting db-config

---
+++ src/main/java/com/checkshow/CheckShowApplication.java
@@ -0,0 +1,13 @@
+package com.checkshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/checkshow/CheckShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
