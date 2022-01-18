00317e2cfe147ae8f7a72963c236b8259e61d4eb
hanhyunsoo <gustn4563@gmail.com>
2022-01-18 11:07:14 +0900

    Edit IntroImage repository, service - saveAll, deleteAll 기능 추가

--- src/main/java/com/checkshow/model/IntroImageService.java
+++ src/main/java/com/checkshow/model/IntroImageService.java
@@ -2,9 +2,14 @@ package com.checkshow.model;
 
 import com.checkshow.dto.request.IntroImageRequest;
 import com.checkshow.entity.IntroImage;
+import com.checkshow.entity.Performance;
 import com.checkshow.repository.IntroImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,8 +17,14 @@ public class IntroImageService {
 
     private final IntroImageRepository introImageRepository;
 
-    public Long save(final IntroImageRequest dto) {
-        IntroImage entity = introImageRepository.save(dto.toEntity());
-        return entity.getId();
+    @Transactional
+    public void saveAll(final List<IntroImageRequest> list) {
+        List<IntroImage> introImages = list.stream().map(IntroImageRequest::toEntity).collect(Collectors.toList());
+        introImageRepository.saveAll(introImages);
+    }
+
+    @Transactional
+    public void deleteAllByPerformance(final Performance performance) {
+        introImageRepository.deleteAllByPerformance(performance);
     }
 }
--- src/main/java/com/checkshow/repository/IntroImageRepository.java
+++ src/main/java/com/checkshow/repository/IntroImageRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface IntroImageRepository extends JpaRepository<IntroImage, Long> {
 
     List<IntroImage> findAllByPerformance(Performance performance);
+
+    void deleteAllByPerformance(Performance performance);
 }
