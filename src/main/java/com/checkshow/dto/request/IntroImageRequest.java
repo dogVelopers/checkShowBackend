12b5cba1717be34947dfaca2f52453fa9ff41c7a
hanhyunsoo <gustn4563@gmail.com>
2022-01-18 11:07:34 +0900

    Edit IntroImageRequest DTO - Builder 기능 추가

--- src/main/java/com/checkshow/dto/request/IntroImageRequest.java
+++ src/main/java/com/checkshow/dto/request/IntroImageRequest.java
@@ -2,11 +2,18 @@ package com.checkshow.dto.request;
 
 import com.checkshow.entity.IntroImage;
 import com.checkshow.entity.Performance;
+import lombok.Builder;
 
 public class IntroImageRequest {
 
     private Performance performance;
     private String url;
+
+    @Builder
+    public IntroImageRequest(Performance performance, String url) {
+        this.performance = performance;
+        this.url = url;
+    }
 
     public IntroImage toEntity() {
         return IntroImage.builder()
