4bc3176828234c5a541d9e9f7d8ffbbdfdccad8d
hanhyunsoo <gustn4563@gmail.com>
2022-01-19 09:09:47 +0900

    Edit Ranking Entity, DTO - Genre 매핑

--- src/main/java/com/checkshow/dto/request/RankingRequest.java
+++ src/main/java/com/checkshow/dto/request/RankingRequest.java
@@ -1,11 +1,12 @@
 package com.checkshow.dto.request;
 
+import com.checkshow.entity.Genre;
 import com.checkshow.entity.Performance;
 
 public class RankingRequest {
 
     private Long id;
     private Performance performance;
-    private String genreCode;
+    private Genre genre;
     private Byte rankNumber;
 }
