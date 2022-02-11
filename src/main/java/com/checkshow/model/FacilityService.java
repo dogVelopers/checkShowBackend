8d6c81168efc5c3e80c9e0b2982797752c096acb
hanhyunsoo <gustn4563@gmail.com>
2022-02-12 01:14:25 +0900

    Add Facility, IntroImage and Ranking Service

--- src/main/java/com/checkshow/model/FacilityService.java
+++ src/main/java/com/checkshow/model/FacilityService.java
@@ -1,9 +1,12 @@
 package com.checkshow.model;
 
 import com.checkshow.dto.request.FacilityRequest;
+import com.checkshow.dto.response.FacilityResponse;
 import com.checkshow.entity.Facility;
 import com.checkshow.repository.FacilityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +20,16 @@ public class FacilityService {
     public String save(final FacilityRequest dto) {
         Facility entity = facilityRepository.save(dto.toEntity());
         return entity.getId();
+    }
+
+    public Page<FacilityResponse> findAll(Pageable pageable) {
+        Page<Facility> page = facilityRepository.findAll(pageable);
+
+        return page.map(FacilityResponse::new);
+    }
+
+    public FacilityResponse findById(final String id) {
+        Facility entity = facilityRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("404"));
+        return new FacilityResponse(entity);
     }
 }
--- src/main/java/com/checkshow/model/IntroImageService.java
+++ src/main/java/com/checkshow/model/IntroImageService.java
@@ -1,9 +1,11 @@
 package com.checkshow.model;
 
 import com.checkshow.dto.request.IntroImageRequest;
+import com.checkshow.dto.response.IntroImageResponse;
 import com.checkshow.entity.IntroImage;
 import com.checkshow.entity.Performance;
 import com.checkshow.repository.IntroImageRepository;
+import com.checkshow.repository.PerformanceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,8 @@ public class IntroImageService {
 
     private final IntroImageRepository introImageRepository;
 
+    private final PerformanceRepository performanceRepository;
+
     @Transactional
     public void saveAll(final List<IntroImageRequest> list) {
         List<IntroImage> introImages = list.stream().map(IntroImageRequest::toEntity).collect(Collectors.toList());
@@ -26,5 +30,12 @@ public class IntroImageService {
     @Transactional
     public void deleteAllByPerformance(final Performance performance) {
         introImageRepository.deleteAllByPerformance(performance);
+    }
+
+    public List<IntroImageResponse> findAllByPerformanceId(final String id) {
+        Performance performance = performanceRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("404"));
+        List<IntroImage> list = introImageRepository.findAllByPerformance(performance);
+
+        return list.stream().map(IntroImageResponse::new).collect(Collectors.toList());
     }
 }
